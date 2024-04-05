e8841781ad794701bf4fcecdb459978f6a7d8cb5
Prasad-Madhuranga <76102607+Prasad-Madhuranga@users.noreply.github.com>
2024-04-05 12:49:58 +0530

    ReformaTed

--- tmp/pwa.js
+++ tmp/pwa.js
@@ -1,73 +1,119 @@
 if (typeof Lazy !== "function") {
     /*! Lazy Function | uses HTML5 localStorage */
-    (function(e) {
-      var t = [];
-  
-      function n(e) { "function" == typeof e && (n.lazied || t.executed ? e.call(window, { type: "LOCAL_STORAGE" }) : t.push(e)) }
-  
-      function o() { 0 == document.documentElement.scrollTop && 0 == document.body.scrollTop || (t.execute({ type: "SCROLL" }), window.removeEventListener("scroll", o)) }
-  
-      function c() { t.execute({ type: "CLICK" }), document.body.removeEventListener("click", c) }
-  
-      function d() { n.lazied || t.executed || (document.body.addEventListener("click", c), window.addEventListener("scroll", o), o()), document.removeEventListener("DOMContentLoaded", d) } t.executed = !1, n.lazied = localStorage.getItem(e.key) === e.value, t.execute = function() { if (!1 === this.executed) { this.executed = !0, n.lazied = !0, localStorage.getItem(e.key) !== e.value && localStorage.setItem(e.key, e.value); for (let e = 0; e < this.length; e++) "function" == typeof this[e] && this[e].apply(window, arguments), this.splice(e, 1), e-- } }, "complete" === document.readyState || "loading" !== document.readyState || null !== document.body ? d() : document.addEventListener("DOMContentLoaded", d), this[e.name] = n
-    }).call(typeof globalThis !== "undefined" ? globalThis : window, { name: "Lazy", key: "LOCAL_LAZY", value: "true" });
-  }
-  
-  (function(app) {
+    (function (e) {
+        var t = [];
+
+        function n(e) {
+            "function" == typeof e &&
+                (n.lazied || t.executed
+                    ? e.call(window, { type: "LOCAL_STORAGE" })
+                    : t.push(e));
+        }
+
+        function o() {
+            (0 == document.documentElement.scrollTop &&
+                0 == document.body.scrollTop) ||
+                (t.execute({ type: "SCROLL" }),
+                    window.removeEventListener("scroll", o));
+        }
+
+        function c() {
+            t.execute({ type: "CLICK" }),
+                document.body.removeEventListener("click", c);
+        }
+
+        function d() {
+            n.lazied ||
+                t.executed ||
+                (document.body.addEventListener("click", c),
+                    window.addEventListener("scroll", o),
+                    o()),
+                document.removeEventListener("DOMContentLoaded", d);
+        }
+        (t.executed = !1),
+            (n.lazied = localStorage.getItem(e.key) === e.value),
+            (t.execute = function () {
+                if (!1 === this.executed) {
+                    (this.executed = !0),
+                        (n.lazied = !0),
+                        localStorage.getItem(e.key) !== e.value &&
+                        localStorage.setItem(e.key, e.value);
+                    for (let e = 0; e < this.length; e++)
+                        "function" == typeof this[e] && this[e].apply(window, arguments),
+                            this.splice(e, 1),
+                            e--;
+                }
+            }),
+            "complete" === document.readyState ||
+                "loading" !== document.readyState ||
+                null !== document.body
+                ? d()
+                : document.addEventListener("DOMContentLoaded", d),
+            (this[e.name] = n);
+    }).call(typeof globalThis !== "undefined" ? globalThis : window, {
+        name: "Lazy",
+        key: "LOCAL_LAZY",
+        value: "true",
+    });
+}
+
+(function (app) {
     /**
      * Return from function
-     * if serviceWorker is not available 
+     * if serviceWorker is not available
      */
     if (!("serviceWorker" in navigator)) {
-      return;
+        return;
     }
-  
+
     /**
      * Helper function to group logs
      */
     const groupLog = (title, logs) => {
-      if (app.consoleLogs === true) {
-        console.groupCollapsed.apply(console, Array.isArray(title) ? title : [title]);
-        logs.forEach(log => console.log.apply(console, Array.isArray(log) ? log : [log]));
-        console.groupEnd();
-      }
-    }
-  
+        if (app.consoleLogs === true) {
+            console.groupCollapsed.apply(
+                console,
+                Array.isArray(title) ? title : [title]
+            );
+            logs.forEach((log) =>
+                console.log.apply(console, Array.isArray(log) ? log : [log])
+            );
+            console.groupEnd();
+        }
+    };
+
     /**
      * Register Workbox Service Worker
      */
     navigator.serviceWorker
-      .register("https://raw.githubusercontent.com/prasad-kmd/Custom-Repo-CDN/main/tmp/sw.js", {
-        scope: "/",
-      })
-      .then((registration) => {
-        const logs = [];
-        if (registration.scope) {
-          logs.push(["Scope: " + registration.scope]);
-        }
-        if (registration.active && registration.active.scriptURL) {
-          logs.push(["Script: " + registration.active.scriptURL]);
-        }
-        logs.push(
-          ["Build by: Universal Blogs"],
-          ["Developer site: https://the-universal-store.blogspot.com/"]
-        );
-  
-        groupLog(
-          [
-            "%cService Worker: Registered Successfully",
-            "color: green"
-          ],
-          logs
-        );
-      })
-      .catch((error) => {
-        groupLog(
-          [
-            "%cService Worker: Registeration Failed",
-            "color: red"
-          ],
-          ["Error:", error]
-        );
-      })
-    });
+        .register(
+            "https://raw.githubusercontent.com/prasad-kmd/Custom-Repo-CDN/main/tmp/sw.js",
+            {
+                scope: "/",
+            }
+        )
+        .then((registration) => {
+            const logs = [];
+            if (registration.scope) {
+                logs.push(["Scope: " + registration.scope]);
+            }
+            if (registration.active && registration.active.scriptURL) {
+                logs.push(["Script: " + registration.active.scriptURL]);
+            }
+            logs.push(
+                ["Build by: Universal Blogs"],
+                ["Developer site: https://the-universal-store.blogspot.com/"]
+            );
+
+            groupLog(
+                ["%cService Worker: Registered Successfully", "color: green"],
+                logs
+            );
+        })
+        .catch((error) => {
+            groupLog(
+                ["%cService Worker: Registeration Failed", "color: red"],
+                ["Error:", error]
+            );
+        });
+});
